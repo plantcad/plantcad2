94175f1cfbb413f5138c611ceb4eaff4ca508cb9
Jingjing Zhai <jz963@cbsugpu08.biohpc.cornell.edu>
2025-03-24 15:13:16 -0400

    Add pipeline for benchmarking pseudo gene task with Evo2

---
+++ pipelines/pseudo_gene/1_data_prep/1_data_prep.sh
@@ -0,0 +1,23 @@
+# extract translation junctions
+ls /workdir/jz963/Expression_modeling/a2z/a2z_dataset/genomes/Zea_mays | while read line
+do
+    assembly=/workdir/jz963/Expression_modeling/a2z/a2z_dataset/genomes/Zea_mays/${line}/assembly.fa
+    gff=$(find /workdir/jz963/Expression_modeling/a2z/a2z_dataset/genomes/NAM_gffs -name "Zm-${line}*.gff3")
+    python $cad2/tasks/pseudo_gene/extract_translation_junctions.py --gff3 "${gff}" --genome "${assembly}" --upstream 4094 --downstream 4095 --prefix "${line}" &
+done
+
+
+# extract splice junctions
+ls /workdir/jz963/Expression_modeling/a2z/a2z_dataset/genomes/Zea_mays | while read line
+do
+    assembly=/workdir/jz963/Expression_modeling/a2z/a2z_dataset/genomes/Zea_mays/${line}/assembly.fa
+    gff=$(find /workdir/jz963/Expression_modeling/a2z/a2z_dataset/genomes/NAM_gffs -name "Zm-${line}*.gff3")
+    python $cad2/tasks/pseudo_gene/extract_splice_junctions.py --gff3 "${gff}" --genome "${assembly}" --upstream 4095 --downstream 4095 --prefix "${line}" &
+done
+
+
+
+ls *fasta | parallel -j 8 'taxa=$(echo {} | sed "s/_.*//g"); \
+prefix=$(basename {} .fasta); \
+python $cad2/tasks/pseudo_gene/generate_labels.py --input {} --taxa $taxa --output ${prefix}_filtered.fasta'
+
--- pipelines/pseudo_gene/3_evo2/Evo2_generate_run.sh
+++ pipelines/pseudo_gene/3_evo2/Evo2_generate_run.sh
@@ -9,7 +9,6 @@ CUDA_VISIBLE_DEVICES=0 python ../../tasks/pseudo_gene/evo2_generate.py --fasta s
 
 # splicing donor site, using the downstream 4095 base pairs as prompts, predict GT
 CUDA_VISIBLE_DEVICES=1 python ../../tasks/pseudo_gene/evo2_generate.py --fasta splice_junctions/B73_donor_filtered.fasta --length 4095 --output splice_junctions/B73_donor_filtered_evo2_7b_ntokens_2 --batch-size 2 --n-tokens 2
-#----------------------------------------------------------------#
 # splicing donor site, using the downstream 4096 base pairs as prompts, G is provided, try to predict T
 CUDA_VISIBLE_DEVICES=0 python ../../tasks/pseudo_gene/evo2_generate.py --fasta splice_junctions/B73_donor_filtered.fasta --length 4096 --output splice_junctions/B73_donor_filtered_evo2_7b_ntokens_1 --batch-size 2 --n-tokens 1
 
@@ -18,3 +17,12 @@ CUDA_VISIBLE_DEVICES=1 python ../../tasks/pseudo_gene/evo2_generate.py --fasta s
 
 # splicing acceptor site, using the upstream 4096 base pairs as prompts, A is provided, try to predict G
 CUDA_VISIBLE_DEVICES=0 python ../../tasks/pseudo_gene/evo2_generate.py --fasta splice_junctions/B73_acceptor_filtered.fasta --length 4096 --output splice_junctions/B73_acceptor_filtered_evo2_7b_ntokens_1 --batch-size 2 --n-tokens 1
+
+# using the upstream 4094 base pairs as prompts, no stop codon provided, predict stop codon
+CUDA_VISIBLE_DEVICES=1 python ../../tasks/pseudo_gene/evo2_generate.py --fasta splice_junctions/B73_stop_sites_filtered.fasta --length 4094 --output splice_junctions/B73_stop_sites_evo2_7b_ntokens_3 --batch-size 2 --n-tokens 3
+
+# using the downstream 4095 base pairs as prompts, T is provided, predict TAA, TAG, TGA
+CUDA_VISIBLE_DEVICES=0 python ../../tasks/pseudo_gene/evo2_generate.py --fasta splice_junctions/B73_stop_sites_filtered.fasta --length 4095 --output splice_junctions/B73_stop_sites_evo2_7b_ntokens_2 --batch-size 2 --n-tokens 2
+
+# using the downstream 4096 base pairs as prompts, T[AG] is provided, try to predict [AG]
+CUDA_VISIBLE_DEVICES=1 python ../../tasks/pseudo_gene/evo2_generate.py --fasta splice_junctions/B73_stop_sites_filtered.fasta --length 4096 --output splice_junctions/B73_stop_sites_evo2_7b_ntokens_1 --batch-size 2 --n-tokens 1
