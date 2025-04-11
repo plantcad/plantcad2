fe148b369c341865cf76138980cc7ff986438aa7
Jingjing Zhai <jz963@cbsumm32.biohpc.cornell.edu>
2025-04-10 20:10:10 -0400

    Fix bugs in running models with l48

--- pipelines/Lloyd_2015_essential_genes/1_plantcad2/run-pcv2-l48-d1024.sh
+++ pipelines/Lloyd_2015_essential_genes/1_plantcad2/run-pcv2-l48-d1024.sh
@@ -1,8 +1,8 @@
 DATA_DIR="/workdir/jz963/utils/plantcad2/results/Lloyd_2015_essential_genes/zero_shot_input/"
 OUTPUT="/workdir/jz963/utils/plantcad2/results/Lloyd_2015_essential_genes/plantcad2_logits/"
 TAXA="Ath"
-CHECKPOINT="kuleshov-group/compo-cad2-l24-dna-chtk-c8192-v2-b2-NpnkD-ba240000"
-PREFIX="pcv2-l24-d0768"
+CHECKPOINT="kuleshov-group/compo-cad2-l48-dna-chtk-c8k-1t-v2d-b2-lr4e4-fcpffa-ba476838"
+PREFIX="pcv2-l28-d1024"
 BATCH_SIZE=16
 
 CUDA_VISIBLE_DEVICES=1 python ../../pseudo_gene/main.py -input ${DATA_DIR}${TAXA}_filtered_tis.fasta \
