a4b30bf797ec6bf14ffd160646e2aa4544ea70e9
Jingjing Zhai <jz963@cbsumm32.biohpc.cornell.edu>
2025-04-10 20:12:48 -0400

    Fix typo

--- pipelines/Lloyd_2015_essential_genes/1_plantcad2/run-pcv2-l48-d1024.sh
+++ pipelines/Lloyd_2015_essential_genes/1_plantcad2/run-pcv2-l48-d1024.sh
@@ -2,7 +2,7 @@ DATA_DIR="/workdir/jz963/utils/plantcad2/results/Lloyd_2015_essential_genes/zero
 OUTPUT="/workdir/jz963/utils/plantcad2/results/Lloyd_2015_essential_genes/plantcad2_logits/"
 TAXA="Ath"
 CHECKPOINT="kuleshov-group/compo-cad2-l48-dna-chtk-c8k-1t-v2d-b2-lr4e4-fcpffa-ba476838"
-PREFIX="pcv2-l28-d1024"
+PREFIX="pcv2-l48-d1024"
 BATCH_SIZE=16
 
 CUDA_VISIBLE_DEVICES=1 python ../../pseudo_gene/main.py -input ${DATA_DIR}${TAXA}_filtered_tis.fasta \
