e9d32f0e83254653ce79266489968e328fca9282
Jingjing Zhai <jz963@cbsugpu08.biohpc.cornell.edu>
2025-05-19 13:57:47 -0400

    Add prediction for maize and barley

--- pipelines/lu_2019_ATACseq/3_sup_cnn-lstm/c600_leave_zma_hvu.sh
+++ pipelines/lu_2019_ATACseq/3_sup_cnn-lstm/c600_leave_zma_hvu.sh
@@ -8,3 +8,14 @@ CUDA_VISIBLE_DEVICES=1 python main.py train  \
     --learning_rate 0.001 \
     --wandb_project PlantCAD2 \
     --balance False
+
+# predict maize and barley
+DATA_DIR=/local/workdir/jz963/utils/plantcad2/results/PlantCAD2_tasks/accessible_angiosperm_c600/data_leave_out_zma_hvu
+CKPT_DIR=${DATA_DIR}/cnn-lstm-imbalance-lr-1e-3
+for species in test_hordeum_vulgare test_zea_mays
+do
+    CUDA_VISIBLE_DEVICES=1 python main.py predict \
+        --test ${DATA_DIR}/${species}.tsv \
+        --model_path ${CKPT_DIR}/bestmodel.ckpt \
+        --output_file ${CKPT_DIR}/${species}_scores.tsv
+done
