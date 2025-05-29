6b4e1670850ae62c0fd056253e6078bb5b85f9a9
Jingjing Zhai <jz963@cbsugpu08.biohpc.cornell.edu>
2025-05-29 14:06:49 -0400

    Update predictions for validation and test

--- pipelines/expression_panand/3_sup_cnn-lstm/run.sh
+++ pipelines/expression_panand/3_sup_cnn-lstm/run.sh
@@ -8,6 +8,18 @@ CUDA_VISIBLE_DEVICES=0 python main.py train --train ${DATA_DIR}/train.tsv \
     --wandb_project PlantCAD2 \
     --task_type classification
 
+python main.py predict --model_path ${OUT_DIR}/bestmodel.ckpt \
+    --test ${DATA_DIR}/test.tsv \
+    --output_file ${OUT_DIR}/predictions.csv \
+    --device cuda:0 \
+    --batch_size 2048
+
+python main.py predict --model_path ${OUT_DIR}/bestmodel.ckpt \
+    --test ${DATA_DIR}/valid.tsv \
+    --output_file ${OUT_DIR}/valid_predictions.csv \
+    --device cuda:0 \
+    --batch_size 2048
+
 DATA_DIR=../../../results/PlantCAD2_tasks/exp-leaf-abs
 OUT_DIR=${DATA_DIR}/cnn_lstm
 CUDA_VISIBLE_DEVICES=0 python main.py train --train ${DATA_DIR}/train.tsv \
@@ -17,3 +29,15 @@ CUDA_VISIBLE_DEVICES=0 python main.py train --train ${DATA_DIR}/train.tsv \
     --learning_rate 0.001 \
     --wandb_project PlantCAD2 \
     --task_type regression
+
+python main.py predict --model_path ${OUT_DIR}/bestmodel.ckpt \
+    --test ${DATA_DIR}/test.tsv \
+    --output_file ${OUT_DIR}/predictions.csv \
+    --device cuda:0 \
+    --batch_size 2048
+
+python main.py predict --model_path ${OUT_DIR}/bestmodel.ckpt \
+    --test ${DATA_DIR}/valid.tsv \
+    --output_file ${OUT_DIR}/valid_predictions.csv \
+    --device cuda:0 \
+    --batch_size 2048
