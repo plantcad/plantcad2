47fc0f2474a3a600eb85a50aadbd10a74ec37acb
Jingjing Zhai <jz963@cbsugpu08.biohpc.cornell.edu>
2025-06-02 14:53:27 -0400

    Finalize predictions

---
+++ pipelines/marand_2021_scATAC/4_sup_plantcad2/run_predict.sh
@@ -0,0 +1,23 @@
+DATA_DIR="../../../results/PlantCAD2_tasks/maize_cell_type_accessible_c300/"
+CUDA_VISIBLE_DEVICES=0 python main.py predict \
+    --data_dir ${DATA_DIR}/test.parquet \
+    --num_labels 92 \
+    --checkpoint_dir ${DATA_DIR}/sup_pcv2-l24-d0768-checkpoints-lr-1e-4/checkpoint-39034 \
+    --output_file ${DATA_DIR}/sup_pcv2-l24-d0768-checkpoints-lr-1e-4/test_predictions.tsv \
+    --batch_size 512
+
+DATA_DIR="../../../results/PlantCAD2_tasks/maize_cell_type_accessible_c600/"
+CUDA_VISIBLE_DEVICES=1 python main.py predict \
+    --data_dir ${DATA_DIR}/test.agront.parquet \
+    --num_labels 92 \
+    --checkpoint_dir ${DATA_DIR}/sup_pcv2-l24-d0768-checkpoints-lr-1e-4/checkpoint-19666 \
+    --output_file ${DATA_DIR}/sup_pcv2-l24-d0768-checkpoints-lr-1e-4/test_predictions.tsv \
+    --batch_size 512
+
+DATA_DIR="../../../results/PlantCAD2_tasks/maize_cell_type_accessible_c1000/"
+CUDA_VISIBLE_DEVICES=0 python main.py predict \
+    --data_dir ${DATA_DIR}/test.agront.parquet \
+    --num_labels 92 \
+    --checkpoint_dir ${DATA_DIR}/sup_pcv2-l24-d0768-checkpoints-lr-1e-4/checkpoint-11807 \
+    --output_file ${DATA_DIR}/sup_pcv2-l24-d0768-checkpoints-lr-1e-4/test_predictions.tsv \
+    --batch_size 512
