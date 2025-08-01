8e82cd0eff1e84a98640d47756fabb0b41befa9e
Jingjing Zhai <jz963@cbsugpu08.biohpc.cornell.edu>
2025-08-01 19:03:22 -0400

    Fix bugs in evo2

--- pipelines/evolutionary_constraint/2_evo2/run.sh
+++ pipelines/evolutionary_constraint/2_evo2/run.sh
@@ -1,5 +1,5 @@
-OUTPUT_DIR=../../../results/evolutionary_constraint/valid_8192_evo2_logits.tsv
-CUDA_VISIBLE_DEVICES=0 python main.py \
+OUTPUT_DIR=../../../results/evolutionary_constraint/valid_8192_evo2_logits_new.tsv
+CUDA_VISIBLE_DEVICES=1 python main.py \
     --output ${OUTPUT_DIR} \
     --input ../../../results/evolutionary_constraint/valid_8192.tsv \
     --batch-size 16 --tokenIdx 4095
