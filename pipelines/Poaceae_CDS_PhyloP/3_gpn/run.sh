e9e5e5c30076fee66d472f643b4e1d9e81ca3c2a
Jingjing Zhai <jz963@cbsugpu08.biohpc.cornell.edu>
2025-08-07 14:53:23 -0400

    Add GPN baseline for poaceae cds conservation

--- pipelines/Poaceae_CDS_PhyloP/3_gpn/run.sh
+++ pipelines/Poaceae_CDS_PhyloP/3_gpn/run.sh
@@ -13,3 +13,48 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     -outLogit ${OUTPUT_DIR} \
     -model 'songlab/gpn-brassicales' \
     -device 'cuda:0'
+
+# for boundary CDS
+## conserved
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_1_gpn.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_1.tsv \
+    -model 'songlab/gpn-brassicales' \
+    -device 'cuda:0'
+
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_2_gpn.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_2.tsv \
+    -model 'songlab/gpn-brassicales' \
+    -device 'cuda:0'
+
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_3_gpn.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_3.tsv \
+    -model 'songlab/gpn-brassicales' \
+    -device 'cuda:0'
+
+## neutral
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_1_gpn.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_1.tsv \
+    -model 'songlab/gpn-brassicales' \
+    -device 'cuda:0'
+
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_2_gpn.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_2.tsv \
+    -model 'songlab/gpn-brassicales' \
+    -device 'cuda:0'
+
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_3_gpn.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_3.tsv \
+    -model 'songlab/gpn-brassicales' \
+    -device 'cuda:0'
