4b00eb1f07c298db5e6bc242c11530769d0b576d
Jingjing Zhai <jz963@cbsugpu08.biohpc.cornell.edu>
2025-08-07 14:53:35 -0400

    Add PDLLM baseline for poaceae cds conservation

--- pipelines/Poaceae_CDS_PhyloP/4_PDLLMs/run.sh
+++ pipelines/Poaceae_CDS_PhyloP/4_PDLLMs/run.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     -input ../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_downsampled.tsv \
     -outLogit ${OUTPUT_DIR} \
     -model 'zhangtaolab/plant-dnamamba-singlebase' \
-    -device 'cuda:0'
+    -device 'cuda:1'
 
 
 OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_downsampled_pdllm_logits.tsv
@@ -11,4 +11,49 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     -input ../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_downsampled.tsv \
     -outLogit ${OUTPUT_DIR} \
     -model 'zhangtaolab/plant-dnamamba-singlebase' \
-    -device 'cuda:0'
+    -device 'cuda:1'
+
+
+
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_1_pdllm.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_1.tsv \
+    -model 'zhangtaolab/plant-dnamamba-singlebase'  \
+    -device 'cuda:1'
+
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_2_pdllm.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_2.tsv \
+    -model 'zhangtaolab/plant-dnamamba-singlebase'  \
+    -device 'cuda:1'
+
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_3_pdllm.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/conserved_8k_TIS_3.tsv \
+    -model 'zhangtaolab/plant-dnamamba-singlebase'  \
+    -device 'cuda:1'
+
+## neutral
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_1_pdllm.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_1.tsv \
+    -model 'zhangtaolab/plant-dnamamba-singlebase'  \
+    -device 'cuda:1'
+
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_2_pdllm.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_2.tsv \
+    -model 'zhangtaolab/plant-dnamamba-singlebase'  \
+    -device 'cuda:1'
+
+OUTPUT_DIR=../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_3_pdllm.tsv
+python main.py \
+    -outLogit ${OUTPUT_DIR} \
+    -input ../../../results/Poaceae_CDS_PhyloP/8k/neutral_8k_TIS_3.tsv \
+    -model 'zhangtaolab/plant-dnamamba-singlebase'  \
+    -device 'cuda:1'
