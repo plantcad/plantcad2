531e4a4226ed8316a224f49e41dba369303d5290
Jingjing Zhai <jz963@cbsugpu08.biohpc.cornell.edu>
2025-11-08 20:40:28 -0500

    Add evaluation metric

--- review/1_benchmark_evo2/evo2.sh
+++ review/1_benchmark_evo2/evo2.sh
@@ -1,42 +1,42 @@
 INPUT_DIR=/workdir/jz963/datasets/PlantCAD2_tasks/fine-tuning/cross_species_acr_train_on_arabidopsis
 OUTPUT_DIR=/local/workdir/jz963/utils/plantcad2/results/review/1_benchmark_evo2/evo2
-CUDA_VISIBLE_DEVICES=0 python main.py fetch \
+CUDA_VISIBLE_DEVICES=0 python evo2.py fetch \
     --input ${INPUT_DIR}/train.tsv \
     --output ${OUTPUT_DIR}/evo2_train_embeddings.pkl \
     --batch-size 4
 
-CUDA_VISIBLE_DEVICES=0 python main.py fetch \
+CUDA_VISIBLE_DEVICES=0 python evo2.py fetch \
     --input ${INPUT_DIR}/valid.tsv \
     --output ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --batch-size 4
 
 # train xgboost
-python main.py train \
+python evo2.py train \
     --train-embeddings ${OUTPUT_DIR}/evo2_train_embeddings.pkl \
     --val-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --strategy forward \
     --output ${OUTPUT_DIR}/xgb_evo2_forward.pkl 
 
-python main.py train \
+python evo2.py train \
     --train-embeddings ${OUTPUT_DIR}/evo2_train_embeddings.pkl \
     --val-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --strategy reverse \
     --output ${OUTPUT_DIR}/xgb_evo2_reverse.pkl &
 
-python main.py train \
+python evo2.py train \
     --train-embeddings ${OUTPUT_DIR}/evo2_train_embeddings.pkl \
     --val-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --strategy average \
     --output ${OUTPUT_DIR}/xgb_evo2_average.pkl &
 
-python main.py train \
+python evo2.py train \
     --train-embeddings ${OUTPUT_DIR}/evo2_train_embeddings.pkl \
     --val-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --strategy concatenate \
     --output ${OUTPUT_DIR}/xgb_evo2_concatenate.pkl &
 
 # train neural network
-CUDA_VISIBLE_DEVICES=1 python main.py train-nn \
+CUDA_VISIBLE_DEVICES=1 python evo2.py train-nn \
     --train-embeddings ${OUTPUT_DIR}/evo2_train_embeddings.pkl \
     --val-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --strategy forward \
@@ -49,7 +49,7 @@ CUDA_VISIBLE_DEVICES=1 python main.py train-nn \
     --weight-decay 0.0 \
     --output ${OUTPUT_DIR}/nn_evo2_forward.pkl &
 
-python main.py train-nn \
+python evo2.py train-nn \
     --train-embeddings ${OUTPUT_DIR}/evo2_train_embeddings.pkl \
     --val-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --strategy reverse \
@@ -62,7 +62,7 @@ python main.py train-nn \
     --weight-decay 0.0 \
     --output ${OUTPUT_DIR}/nn_evo2_reverse.pkl &
 
-CUDA_VISIBLE_DEVICES=1 python main.py train-nn \
+CUDA_VISIBLE_DEVICES=1 python evo2.py train-nn \
     --train-embeddings ${OUTPUT_DIR}/evo2_train_embeddings.pkl \
     --val-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --strategy average \
@@ -74,7 +74,7 @@ CUDA_VISIBLE_DEVICES=1 python main.py train-nn \
     --weight-decay 0.0 \
     --output ${OUTPUT_DIR}/nn_evo2_average.pkl &
 
-CUDA_VISIBLE_DEVICES=1 python main.py train-nn \
+CUDA_VISIBLE_DEVICES=1 python evo2.py train-nn \
     --train-embeddings ${OUTPUT_DIR}/evo2_train_embeddings.pkl \
     --val-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --strategy concatenate \
@@ -88,43 +88,43 @@ CUDA_VISIBLE_DEVICES=1 python main.py train-nn \
     --output ${OUTPUT_DIR}/nn_evo2_concatenate.pkl &
 
 # evaluate xgboost
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --model ${OUTPUT_DIR}/xgb_evo2_forward.pkl \
     --output ${OUTPUT_DIR}/xgb_evo2_forward_results.tsv
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --model ${OUTPUT_DIR}/xgb_evo2_reverse.pkl \
     --output ${OUTPUT_DIR}/xgb_evo2_reverse_results.tsv 
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --model ${OUTPUT_DIR}/xgb_evo2_average.pkl \
     --output ${OUTPUT_DIR}/xgb_evo2_average_results.tsv 
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --model ${OUTPUT_DIR}/xgb_evo2_concatenate.pkl \
     --output ${OUTPUT_DIR}/xgb_evo2_concatenate_results.tsv 
 
 # evaluate neural network
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --model ${OUTPUT_DIR}/nn_evo2_forward.pkl \
     --output ${OUTPUT_DIR}/nn_evo2_forward_results.tsv  
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --model ${OUTPUT_DIR}/nn_evo2_reverse.pkl \ 
     --output ${OUTPUT_DIR}/nn_evo2_reverse_results.tsv
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --model ${OUTPUT_DIR}/nn_evo2_average.pkl \
     --output ${OUTPUT_DIR}/nn_evo2_average_results.tsv
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${OUTPUT_DIR}/evo2_valid_embeddings.pkl \
     --model ${OUTPUT_DIR}/nn_evo2_concatenate.pkl \
     --output ${OUTPUT_DIR}/nn_evo2_concatenate_results.tsv  
--- review/1_benchmark_evo2/evo2_test_populus_trichocarpa.sh
+++ review/1_benchmark_evo2/evo2_test_populus_trichocarpa.sh
@@ -2,43 +2,43 @@ OUTPUT_DIR=/local/workdir/jz963/utils/plantcad2/results/review/1_benchmark_evo2/
 TEST_EMBEDDINGS=${OUTPUT_DIR}/test_populus_trichocarpa_evo2.pkl
 
 # evaluate xgboost
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${TEST_EMBEDDINGS} \
     --model ${OUTPUT_DIR}/xgb_evo2_forward.pkl \
     --output ${OUTPUT_DIR}/xgb_evo2_forward_populus_trichocarpa_results.tsv
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${TEST_EMBEDDINGS} \
     --model ${OUTPUT_DIR}/xgb_evo2_reverse.pkl \
     --output ${OUTPUT_DIR}/xgb_evo2_reverse_populus_trichocarpa_results.tsv
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${TEST_EMBEDDINGS} \
     --model ${OUTPUT_DIR}/xgb_evo2_average.pkl \
     --output ${OUTPUT_DIR}/xgb_evo2_average_populus_trichocarpa_results.tsv
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${TEST_EMBEDDINGS} \
     --model ${OUTPUT_DIR}/xgb_evo2_concatenate.pkl \
     --output ${OUTPUT_DIR}/xgb_evo2_concatenate_populus_trichocarpa_results.tsv
 
 # evaluate neural network
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${TEST_EMBEDDINGS} \
     --model ${OUTPUT_DIR}/nn_evo2_forward.pkl \
     --output ${OUTPUT_DIR}/nn_evo2_forward_populus_trichocarpa_results.tsv
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${TEST_EMBEDDINGS} \
     --model ${OUTPUT_DIR}/nn_evo2_reverse.pkl \
     --output ${OUTPUT_DIR}/nn_evo2_reverse_populus_trichocarpa_results.tsv
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${TEST_EMBEDDINGS} \
     --model ${OUTPUT_DIR}/nn_evo2_average.pkl \
     --output ${OUTPUT_DIR}/nn_evo2_average_populus_trichocarpa_results.tsv
 
-python main.py evaluate \
+python evo2.py evaluate \
     --test-embeddings ${TEST_EMBEDDINGS} \
     --model ${OUTPUT_DIR}/nn_evo2_concatenate.pkl \
     --output ${OUTPUT_DIR}/nn_evo2_concatenate_populus_trichocarpa_results.tsv
